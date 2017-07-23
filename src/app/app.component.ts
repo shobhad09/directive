42aa40a6d43aaa0bd6231132943dc9831b47a0a2
shobhad09 <shobha@tenettech.com>
2017-07-23 15:18:40 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-component',
+  templateUrl: 'app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class RootComponent {
+
+  employeename: Employee[] = [];
+  ngOnInit() {
+    this.employeename = [{
+      name: "Sachin",
+      lastName: 'gowda'
+    },
+    {
+      name: "ashish",
+      lastName: 'Dhoni'
+    },
+    {
+      name: "deepa",
+      lastName: 'raj'
+    }]
+
+  }
+}
+
+interface Employee {
+  name: string;
+  lastName: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { RootComponent } from './app.component';
+import { HighLighterDirective } from './appdirective';
 
 @NgModule({
   declarations: [
-    AppComponent
+    RootComponent,
+    HighLighterDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [RootComponent]
 })
 export class AppModule { }
